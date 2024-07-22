33a8221d319d49ee2a4aabb3367c379b3d2d57dd
delt4d <leandrocruz717@outlook.com>
2024-07-22 18:23:15 -0300

    style: format code

--- back-end/src/example/routes.ts
+++ back-end/src/example/routes.ts
@@ -3,8 +3,8 @@ import * as controller from './controller';
 
 const routes = Router();
 
-routes.get('', controller.index);
-routes.post('', controller.createItem);
+routes.get("", controller.index);
+routes.post("", controller.createItem);
 routes.get('/:id', controller.getItem);
 routes.put('/:id', controller.updateItem);
 routes.delete('/:id', controller.deleteItem);
