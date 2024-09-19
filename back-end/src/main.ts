0b1966dc4a8f5fb5489fe2d0f032c2fe27c69ab4
Luis Otávio <131190540+luisotavio2023@users.noreply.github.com>
2024-09-19 19:20:34 +0000

    Email service concluído

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -2,8 +2,10 @@ import { DatabaseResolver } from './database';
 import project from './config/project';
 import app from './app';
 import EmailService from './email/service';
+import { UserRole } from './user/model';
 
 app.listen(project.port, async () => {
     await DatabaseResolver.getConnection();
     console.log('Server running at port ' + project.port);
 });
+
