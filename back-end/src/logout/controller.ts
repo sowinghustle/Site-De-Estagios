53f496dbff914ce994d9fc73fd806dcf3e445969
delta4d <leandrocruz717@outlook.com>
2024-09-26 15:44:02 -0300

    refactor(logout): send error directly instead of sending the error to express next object

--- back-end/src/logout/controller.ts
+++ back-end/src/logout/controller.ts
@@ -4,10 +4,11 @@ import logoutService from './service';
 export default class LogoutController {
     async logout(req: Request, res: Response, next: NextFunction) {
         if (!req.token) {
-            const error = new Error(
-                'Não foi possível deslogar do sistema porque o token de acesso não foi recebido.'
-            );
-            return next(error);
+            return res.status(400).send({
+                success: false,
+                message:
+                    'Não foi possível deslogar do sistema porque o token de acesso não foi recebido.',
+            });
         }
 
         const logoutResult = await logoutService.handle(req.token);
