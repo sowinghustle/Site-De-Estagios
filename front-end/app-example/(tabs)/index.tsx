72eeeae98622d9e6f097a8d1a6d5cc320374e7e6
SowingHustle <hazewry00@gmail.com>
2024-09-27 08:01:04 -0300

    refactor: change the app structure

--- front-end/app-example/(tabs)/index.tsx
+++ front-end/app-example/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { View } from 'react-native';
+import { Link } from 'expo-router';
 import CustomButton from '@/components/button/CustomButton';
 
 export default function HomeScreen() {
   return (
     <View>
+      <Link href='/signup/'>Registre-se</Link>
+      <Link href='/signin/'>Entrar</Link>
       <CustomButton title="PrimaryButton" type="primary" />
       <CustomButton title="SecondaryButton" type="secondary" />
     </View>
